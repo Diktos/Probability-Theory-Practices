bd51e0d291b3ee56b9b2c1890f6a7043701d9454
User <User@DESKTOP-AT2CFBF>
2026-09-10 10:59:56 +0300

    add block3

---
+++ Practice1/Practice1/block3.py
@@ -0,0 +1,6 @@
+# Блок 3. Оцінка числа Пі (Монте-Карло)
+M_pi = sum(1 for _ in range(N_block2) if random.uniform(-1, 1)**2 + random.uniform(-1, 1)**2 <= 1.0)
+pi_est = 4.0 * M_pi / N_block2 
+
+print(f"\nПі (Монте-Карло): {pi_est:.5f}")
+print(f"Похибка: {abs(pi_est - math.pi):.5f}")
