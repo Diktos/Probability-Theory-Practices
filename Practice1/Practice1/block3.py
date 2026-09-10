a677bf87b552ecf5c67e10da1107d125a73b8b37
User <User@DESKTOP-AT2CFBF>
2026-09-10 11:01:43 +0300

    add block4_4variant

--- Practice1/Practice1/block3.py
+++ Practice1/Practice1/block3.py
@@ -1,4 +1,8 @@
 # Блок 3. Оцінка числа Пі (Монте-Карло)
+
+import random
+import math
+
 M_pi = sum(1 for _ in range(N_block2) if random.uniform(-1, 1)**2 + random.uniform(-1, 1)**2 <= 1.0)
 pi_est = 4.0 * M_pi / N_block2 
 
--- Practice1/Practice1/block4_4v.py
+++ Practice1/Practice1/block4_4v.py
@@ -1,1 +1,8 @@
 
+import random
+import math
+
+# Блок 4. Варіант 4. Класифікатор
+total, correct, error = 5000, 4600, 400 
+print(f"\nP(Correct) = {correct / total:.3f}")
+print(f"P(Error) = {error / total:.3f}")
